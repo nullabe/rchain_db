3820c9cd7190b9f01cf0a5e6f51a134ad7e07946
nullabe <abelluard@outlook.com>
2020-04-05 19:38:17 +0200

    refactor: Add Default impl for Block and Blochain structs

--- src/db/block.rs
+++ src/db/block.rs
@@ -1,12 +1,18 @@
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct Block {
     hash: Option<String>,
 }
 
 impl Block {
-    pub fn new() -> Block {
+    pub fn new() -> Self {
         Block { hash: None }
     }
 
     pub fn hash(&self) {}
+}
+
+impl Default for Block {
+    fn default() -> Self {
+        Block::new()
+    }
 }
