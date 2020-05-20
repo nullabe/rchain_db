397626c4588b629a5a28d6e13b4a32209be0ceb0
nullabe <abelluard@outlook.com>
2020-05-21 01:12:24 +0200

    build: Update

--- src/storage/blockchain.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::MutexGuard;
-
-use crate::crypto::Sha256Blockchain;
-
-pub trait BlockchainStorage {
-    fn retrieve(&self) -> Sha256Blockchain;
-
-    fn persist(&self, blockchain: MutexGuard<Sha256Blockchain>);
-}
